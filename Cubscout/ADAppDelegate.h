2a02b34e500809fc2d8336880b5559c5d9df3899
Andrew Yates <contact@andydev.co.uk>
2014-03-04 09:12:04 -0800

    Layout main window and allow API key to be saved using SSKeychain

--- Cubscout/ADAppDelegate.h
+++ Cubscout/ADAppDelegate.h
@@ -8,8 +8,18 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface ADAppDelegate : NSObject <NSApplicationDelegate>
+@interface ADAppDelegate : NSObject <NSApplicationDelegate, NSUserNotificationCenterDelegate>
 
+// Status Item
+@property (strong, nonatomic) NSStatusItem *statusItem;
+
+// Main Window
 @property (assign) IBOutlet NSWindow *window;
+@property (retain, nonatomic) IBOutlet NSTextField *hsAPIKeyField;
+@property (retain, nonatomic) IBOutlet NSButton *saveBtn;
+@property (retain, nonatomic) IBOutlet NSButton *closeBtn;
+
+// Agent
+@property (strong, nonatomic) NSMutableArray *hsMailboxes;
 
 @end
--- Cubscout/ADAppDelegate.m
+++ Cubscout/ADAppDelegate.m
@@ -7,12 +7,51 @@
 //
 
 #import "ADAppDelegate.h"
+#import "ADStatusItem.h"
+#import <SSKeychain/SSKeychain.h>
+
+// Services
+#import "ADMailboxesService.h"
+#import "ADConversationsService.h"
 
 @implementation ADAppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
+@synthesize statusItem;
+@synthesize hsAPIKeyField, saveBtn, closeBtn;
+@synthesize hsMailboxes;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    
+    // Set up Status Bar Item
+    NSStatusItem *aStatusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength];
+    ADStatusItem *view = [[ADStatusItem alloc] init];
+    [aStatusItem setView:view];
+    [self setStatusItem:aStatusItem];
+    
+    // Check for password and fetch mailboxes etc.
+    if([SSKeychain passwordForService:@"Cubscout" account:@"Cubscout"]){
+        [self getMailboxes];
+        [self getConversations];
+    }
+    
+}
+
+#pragma mark - Main Window
+
+- (IBAction)onButtonClick:(id)sender {
+    if(sender == saveBtn){
+        [self saveSettings];
+    } else if(sender == closeBtn) {
+        [NSApp terminate:self];
+    }
+}
+
+- (void)saveSettings {
+    NSError *error = nil;
+    
+    [SSKeychain setPassword:[hsAPIKeyField stringValue] forService:@"Cubscout" account:@"Cubscout" error:&error];
+}
+
 }
 
 @end
